b6f1b4d43f2610e599ac07f74064ab37dd032c11
nickzitzer <nicholas.zitzer@live.com>
2024-08-14 14:02:43 -0400

    fixing next config js to support production routes

--- elevate-frontend/next.config.js
+++ elevate-frontend/next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
       return [
         {
           source: '/api/:path*',
-          destination: 'http://localhost:3001/api/:path*', // Proxy to Backend
+          destination: 'http://localhost:3001/api/:path*', // Proxy to local Backend
         },
-      ]
-    },
-  };
+      ];
+    } else {
+      return [
+        {
+          source: '/api/:path*',
+          destination: '/api/:path*', // Use Vercel's routing in production
+        },
+      ];
+    }
+  },
+};
 
 module.exports = nextConfig;
